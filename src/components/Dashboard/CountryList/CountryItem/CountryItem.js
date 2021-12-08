e58adebf0eaec19affca33bf3d8f266f22cefbd7
Sina Moraddar <sinamoraddar@gmail.com>
2021-12-08 22:25:13 +0330

    create the CountryItem component

---
+++ src/components/Dashboard/CountryList/CountryItem/CountryItem.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Numeral from "numeral";
+import { Link } from "react-router-dom";
+import styles from "./CountryItem.module.scss";
+
+const CountryItem = ({
+  capital,
+  alpha2Code,
+  darkMode,
+  name,
+  population,
+  region,
+  homePage
+}) => {
+  return (
+    <Link to={`${homePage}countries/${name}`}>
+      <figure
+        className={`${styles.countryItem} ${
+          darkMode ? `dark darkElements` : `light lightElements`
+        }`}
+      >
+        <div
+          className={styles.background}
+          style={{
+            background: `url(https://cdn.rawgit.com/hjnilsson/country-flags/master/svg/${alpha2Code.toLowerCase()}.svg)`
+          }}
+        />
+        <figcaption className={styles.textContainer}>
+          <h2>{name}</h2>
+          <div>
+            Population :
+            <span
+              className={darkMode ? styles.darkCaption : styles.lightCaption}
+            >
+              {/* format the population number via numeral.js package */}
+              {Numeral(population).format(0, 0)}
+            </span>
+          </div>
+          <div>
+            Region :
+            <span
+              className={darkMode ? styles.darkCaption : styles.lightCaption}
+            >
+              {region}
+            </span>
+          </div>
+          <div>
+            Capital :
+            <span
+              className={darkMode ? styles.darkCaption : styles.lightCaption}
+            >
+              {capital || "-"}
+            </span>
+          </div>
+        </figcaption>
+      </figure>
+    </Link>
+  );
+};
+
+export default CountryItem;
