49c2ee478ea53482582c6642b23358438c8e1947
Sina Moraddar <sinamoraddar@gmail.com>
2021-12-09 11:48:46 +0330

    turn App.js into a functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,89 +1,83 @@
-import React, { Component } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Dashboard from "./Dashboard/Dashboard";
 import CountryDetails from "./CountryDetails/CountryDetails";
 import NotFound from "./NotFoundPage/NotFound";
+import { AppContext } from "../contexts/AppContext";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    //determine the homepage path based on the development mode
-    this.homePage =
-      process.env.NODE_ENV === "development"
-        ? "/"
-        : "/REST-Countries-API-with-color-theme-switcher/";
-  }
-  state = {
-    darkMode: true,
-    totalCountries: []
-  };
-  //change the darkmode state based on user interaction
-  appModeChanger = () => {
-    this.setState(prevState => ({ darkMode: !prevState.darkMode }));
-  };
+//determine the homepage path based on the development mode
+const homePage =
+  process.env.NODE_ENV === "development"
+    ? "/"
+    : "/REST-Countries-API-with-color-theme-switcher/";
 
-  componentDidMount() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [isUsingDarkMode, setIsUsingDarkMode] = useState(true);
+  const [totalCountries, setTotalCountries] = useState([]);
+  // //change the darkmode state based on user interaction
+  const appModeChanger = useCallback(() => {
+    setIsUsingDarkMode((isUsingDarkMode) => !isUsingDarkMode);
+  }, []);
+
+  useEffect(() => {
     // fetch the countrylist's data from the api
     // we saved the json file in another repository on github cause the original api was broken somehow
     axios
-      .get(
-'https://restcountries.com/v2/all'      )
-      .then(res => {
-        this.setState(() => ({
-          totalCountries: res.data
-        }));
+      .get("https://restcountries.com/v2/all")
+      .then((res) => {
+        setTotalCountries(res.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  }
+  }, []);
 
-  render() {
-    const { darkMode, totalCountries } = this.state;
-    return (
+  return (
+    <AppContext.Provider value={{ loading, setLoading }}>
       <BrowserRouter>
         <Switch>
           <Route
             exact
-            path={this.homePage}
-            render={routeProps => (
+            path={homePage}
+            render={(routeProps) => (
               <Dashboard
                 {...routeProps}
-                appModeChanger={this.appModeChanger}
-                darkMode={darkMode}
-                homePage={this.homePage}
-                totalCountries={totalCountries}
-              />
-            )}
-         />
-          <Route
-            exact
-            path={`${this.homePage}countries/:countryName`}
-            render={routeProps => (
-              <CountryDetails
-                {...routeProps}
-                darkMode={darkMode}
-                homePage={this.homePage}
-                appModeChanger={this.appModeChanger}
+                appModeChanger={appModeChanger}
+                darkMode={isUsingDarkMode}
+                homePage={homePage}
                 totalCountries={totalCountries}
               />
             )}
           />
           <Route
-            render={routeProps => (
+            exact
+            path={`${homePage}countries/:countryName`}
+            render={(routeProps) => (
+              <CountryDetails
+                {...routeProps}
+                darkMode={isUsingDarkMode}
+                homePage={homePage}
+                appModeChanger={appModeChanger}
+                totalCountries={totalCountries}
+              />
+            )}
+          />
+          <Route
+            render={(routeProps) => (
               <NotFound
                 {...routeProps}
-                darkMode={darkMode}
-                homePage={this.homePage}
+                darkMode={isUsingDarkMode}
+                homePage={homePage}
               />
             )}
           />
         </Switch>
       </BrowserRouter>
-    );
-  }
-}
+    </AppContext.Provider>
+  );
+};
 
 export default App;
